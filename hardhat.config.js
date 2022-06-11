b263d7ffbe61acc59af59d79d367113784e59086
mansijoshi17 <mansijoshi1777@gmail.com>
2022-06-11 15:20:16 +0530

    Configration on Harmony network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "./.env" });
 require("@nomiclabs/hardhat-waffle");
 
 const pk_1 = process.env.REACT_APP_PRIVATE_KEY;
+
 console.log(pk_1);
 module.exports = {
   solidity: "0.8.4",
@@ -14,6 +15,10 @@ module.exports = {
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}`,
       accounts: [pk_1],
+    },
+    harmony: {
+      url: `https://api.s0.b.hmny.io`,
+      accounts: [`0x${pk_1}`]
     },
   },
 };
