361d003f4e496215edb3acc1fc03c23117768553
mansijoshi17 <mansijoshi1777@gmail.com>
2022-06-11 15:56:34 +0530

    Changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,14 +8,6 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     hardhat: {},
-    boba_rinkeby: {
-      url: "https://rinkeby.boba.network",
-      accounts: [pk_1],
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}`,
-      accounts: [pk_1],
-    },
     harmony: {
       url: `https://api.s0.b.hmny.io`,
       accounts: [`0x${pk_1}`]
